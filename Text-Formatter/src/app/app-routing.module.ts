f072b21be8c56bd0b2ccabbb02f22e4f14dce476
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-07-03 19:50:03 +0530

    SK | Created home, text-display, formatters components and added routing to home page

--- Text-Formatter/src/app/app-routing.module.ts
+++ Text-Formatter/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TextDisplayComponent } from './home/text-display/text-display.component';
+import { FormattersComponent } from './home/formatters/formatters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'text-display', component: TextDisplayComponent },
+  { path: 'formatters', component: FormattersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
