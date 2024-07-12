96a3e156ea0696ac8f60a9389d10252a1a769057
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-07-12 12:29:16 +0530

    Completed Assignment3

--- Text-Formatter/src/app/modules/apis-component/apis-component.component.ts
+++ Text-Formatter/src/app/modules/apis-component/apis-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { JsonplaceholderService } from './services/jsonplaceholder.service';
 
 @Component({
@@ -6,121 +6,106 @@ import { JsonplaceholderService } from './services/jsonplaceholder.service';
   templateUrl: './apis-component.component.html',
   styleUrls: ['./apis-component.component.scss']
 })
-export class APIsComponentComponent  {
-
+export class APIsComponentComponent {
   userId: string = '';
   inputBody: string = '';
   errorMessage: string = '';
   responseData: any = [];
   isPhotosResponse: boolean = false;
 
-  constructor(private jsonplaceholderService: JsonplaceholderService) { }
+  constructor(private jsonplaceholderService: JsonplaceholderService) {}
 
-  // ngOnInit(): void {
-  // }
-
-  fetchUserInfo() {
+  fetchData(type: string) {
     this.clearError();
-    if(!this.userId) {
-      this.errorMessage = 'User ID should be not empty!';
+    if (!this.userId) {
+      this.errorMessage = 'User ID should not be empty!';
       return;
     }
-    this.jsonplaceholderService.getUserInfo(Number(this.userId)).subscribe(
-      data => {
-        this.isPhotosResponse = false;
-        this.responseData = data;
-      },
-      error => this.errorMessage = error.message
-    );
-  }
 
-  fetchPhotos() {
-    this.clearError();
-    if(!this.userId) {
-      this.errorMessage = 'User ID should be not empty!';
-      return;
+    switch (type) {
+      case 'comments':
+        this.jsonplaceholderService.getUserComments(Number(this.userId)).subscribe(
+          (data: any) => {
+            this.isPhotosResponse = false;
+            this.responseData = data;
+          },
+          (error: any) => this.errorMessage = error.message
+        );
+        break;
+      case 'albums':
+        this.jsonplaceholderService.getUserAlbums(Number(this.userId)).subscribe(
+          (data: any) => {
+            this.isPhotosResponse = false;
+            this.responseData = data;
+          },
+          (error: any) => this.errorMessage = error.message
+        );
+        break;
+      case 'photos':
+        this.jsonplaceholderService.getUserPhotos(Number(this.userId)).subscribe(
+          (data: any) => {
+            this.isPhotosResponse = true;
+            this.responseData = data;
+          },
+          (error: any) => this.errorMessage = error.message
+        );
+        break;
+      case 'todos':
+        this.jsonplaceholderService.getUserTodos(Number(this.userId)).subscribe(
+          (data: any) => {
+            this.isPhotosResponse = false;
+            this.responseData = data;
+          },
+          (error: any) => this.errorMessage = error.message
+        );
+        break;
+      default:
+        break;
     }
-    this.jsonplaceholderService.getUserPhotos(Number(this.userId)).subscribe(
-      data => {
-        this.isPhotosResponse = true;
-        this.responseData = data;
-      },
-      error => this.errorMessage = error.message
-    );
-  }
-
-  fetchAlbums() {
-    this.clearError();
-    if(!this.userId) {
-      this.errorMessage = 'User ID should be not empty!';
-      return;
-    }
-    this.jsonplaceholderService.getUserAlbums(Number(this.userId)).subscribe(
-      data => {
-        this.isPhotosResponse = false;
-        this.responseData = data;
-      },
-      error => this.errorMessage = error.message
-    );
-  }
-
-  fetchTodos() {
-    this.clearError();
-    if(!this.userId) {
-      this.errorMessage = 'User ID should be not empty!';
-      return;
-    }
-    this.jsonplaceholderService.getTodos(Number(this.userId)).subscribe(
-      data => {
-        this.isPhotosResponse = false;
-        this.responseData = data;
-      },
-      error => this.errorMessage = error.message
-    );
   }
 
   createUser() {
     this.clearError();
-    if(!this.inputBody) {
-      this.errorMessage = 'Input body should be not empty!';
+    if (!this.inputBody) {
+      this.errorMessage = 'Input body should not be empty!';
       return;
     }
     this.jsonplaceholderService.createUser({ body: this.inputBody }).subscribe(
-      data => {
+      (data: any) => {
         this.isPhotosResponse = false;
         this.responseData = data;
       },
-      error => this.errorMessage = error.message
+      (error: any) => this.errorMessage = error.message
     );
   }
 
   updateUser() {
     this.clearError();
-    if(!this.userId || !this.inputBody) {
-      this.errorMessage = 'User ID and Input Body should be not empty!';
+    if (!this.userId || !this.inputBody) {
+      this.errorMessage = 'User ID and Input Body should not be empty!';
       return;
     }
     this.jsonplaceholderService.updateUser(Number(this.userId), { body: this.inputBody }).subscribe(
-      data => {
+      (data: any) => {
         this.isPhotosResponse = false;
         this.responseData = data;
       },
-      error => this.errorMessage = error.message
+      (error: any) => this.errorMessage = error.message
     );
   }
 
   deleteUser() {
     this.clearError();
-    if(!this.userId) {
-      this.errorMessage = 'User ID should be not empty!';
+    if (!this.userId) {
+      this.errorMessage = 'User ID should not be empty!';
       return;
     }
     this.jsonplaceholderService.deleteUser(Number(this.userId)).subscribe(
-      data => {
+      (data: any) => {
         this.isPhotosResponse = false;
         this.responseData = data;
       },
-      error => this.errorMessage = error.message
+      (error: any) => this.errorMessage = error.message
     );
   }
 
@@ -129,7 +114,7 @@ export class APIsComponentComponent  {
   }
 
   getKeys(obj: any): string[] {
-    return obj?Object.keys(obj): [];
+    return obj ? Object.keys(obj) : [];
   }
 
   isObject(value: any): boolean {
--- Text-Formatter/src/app/modules/apis-component/services/jsonplaceholder.service.ts
+++ Text-Formatter/src/app/modules/apis-component/services/jsonplaceholder.service.ts
@@ -7,47 +7,47 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class JsonplaceholderService {
-
   private apiUrl = 'https://jsonplaceholder.typicode.com';
 
   constructor(private http: HttpClient) { }
 
+  private handleError(error: any) {
+    return throwError(error);
+  }
+
   getUserInfo(userId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/users/${userId}`)
-    .pipe(catchError(this.handleError));
+    return this.http.get(`${this.apiUrl}/users/${userId}`).pipe(catchError(this.handleError));
   }
 
   getUserPhotos(userId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/photos?userId=${userId}`)
-    .pipe(catchError(this.handleError));
+    return this.http.get(`${this.apiUrl}/photos?userId=${userId}`).pipe(catchError(this.handleError));
   }
 
   getUserAlbums(userId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/albums?userId=${userId}`)
-    .pipe(catchError(this.handleError));
+    return this.http.get(`${this.apiUrl}/albums?userId=${userId}`).pipe(catchError(this.handleError));
   }
 
   getTodos(userId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/todo?userId=${userId}`)
-    .pipe(catchError(this.handleError));
+    return this.http.get(`${this.apiUrl}/todos?userId=${userId}`).pipe(catchError(this.handleError));
   }
 
   createUser(data: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/users`, data)
-    .pipe(catchError(this.handleError));
+    return this.http.post(`${this.apiUrl}/users`, data).pipe(catchError(this.handleError));
   }
 
   updateUser(userId: number, data: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/users/${userId}`, data)
-    .pipe(catchError(this.handleError));
+    return this.http.put(`${this.apiUrl}/users/${userId}`, data).pipe(catchError(this.handleError));
   }
 
   deleteUser(userId: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/users/${userId}`)
-    .pipe(catchError(this.handleError));
+    return this.http.delete(`${this.apiUrl}/users/${userId}`).pipe(catchError(this.handleError));
   }
 
-  private handleError(error: any) {
-    return throwError(error);
+  getUserComments(userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/comments?userId=${userId}`).pipe(catchError(this.handleError));
+  }
+
+  getUserTodos(userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/todos?userId=${userId}`).pipe(catchError(this.handleError));
   }
 }
